cd5019522095d78cc18507924542aa62b33402ed
Ezra Raez <ezRAez10@gmail.com>
2015-12-12 10:19:16 -0800

    update db

--- app/config/database.js
+++ app/config/database.js
@@ -2,15 +2,11 @@ var mongoose = require('mongoose');
 
 var db = mongoose.connection;
 
-var dbUri = process.env.MONGOLAB_URI // ||
-            // 'mongodb://localhost/' + env.SAFE_TITLE;
+var dbUri = process.env.MONGOLAB_URI  ||
+            'mongodb://localhost/' + env.SAFE_TITLE;
 
 // connect to db
 mongoose.connect(dbUri);
-
-db.on('connected', function() {
-  console.log("Connected to db: " + dbUri)
-})
 
 
 // export the connection
